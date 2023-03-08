3c2a70caa8bc721be48807c893121fcdc5f22a26
Víctor Araque <victor.araque@ogilvy.com>
2023-03-08 12:22:15 +0100

    reset

--- pkg/model/fuzziemon.go
+++ pkg/model/fuzziemon.go
@@ -1,20 +1,20 @@
 package model
 
-
 type Fuzziemon struct {
-    Id               string
-	Name             string
-	EntryDescription string
-	EntryNumber      uint
-	Stats            [6]uint8
+	Id               string        // A unique identifier for the Fuzziemon
+	EntryNumber      uint          // The number assigned to the Fuzziemon in the fuzziedeck
+	Name             string        // The name of the Fuzziemon
+	EntryDescription string        // A brief description of the Fuzziemon in the fuzziedeck
+	Stats            [6]uint8      // The Fuzziemon's attributes.
 }
 
-func NewFuzziemon(id string, name string, entryDescription string, entryNumber uint, stats [6]uint8) *Fuzziemon {
-	return &Fuzziemon {
-        Id:               id,
+// NewFuzziemon creates a new instance of Fuzziemon with the given values
+func NewFuzziemon(id string, entryNumber uint, name string, entryDescription string, stats [6]uint8) *Fuzziemon {
+	return &Fuzziemon{
+		Id:               id,
+		EntryNumber:      entryNumber,
 		Name:             name,
 		EntryDescription: entryDescription,
-		EntryNumber:      entryNumber,
 		Stats:            stats,
 	}
 }
